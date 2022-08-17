80f21840de3ba46a45f33d6a2d70ff71a95dab3b
SamuelM1010 <110546631+SamuelM1010@users.noreply.github.com>
2022-08-17 21:53:45 +0100

    Added A check Rounding method
    Makes sure data is exactly 3dp

--- csvValidator.py
+++ csvValidator.py
@@ -1,5 +1,6 @@
 import csv
-batchIDs=[]
+
+batchIDs = []
 
 f = open("Samples - Invalid\Duplicate Batch Numbers\MED_DATA_20220803153918.csv", "rt")
 ##reader = csv.reader(f)
@@ -8,6 +9,8 @@ f = open("Samples - Invalid\Duplicate Batch Numbers\MED_DATA_20220803153918.csv"
 
 data = list(csv.reader(f))
 f.close()
+
+
 ##print(data[0][0])
 
 def isFloat(num):
@@ -17,46 +20,63 @@ def isFloat(num):
     except ValueError:
         return False
 
+
 def printFile():
     for row in data:
         print(row)
 
+
 def checkHeaders():
     print(data[0])
-    headerTemplate = ['batch_id', 'timestamp', 'reading1', 'reading2', 'reading3', 'reading4', 'reading5', 'reading6', 'reading7', 'reading8', 'reading9', 'reading10']
+    headerTemplate = ['batch_id', 'timestamp', 'reading1', 'reading2', 'reading3', 'reading4', 'reading5', 'reading6',
+                      'reading7', 'reading8', 'reading9', 'reading10']
     if data[0] == headerTemplate:
         print("VALID HEADERS")
     else:
         print("INVALID HEADERS")
         data[0] = headerTemplate
+
+
 ##        checkHeaders()
 
 def checkBatchIDDuplicates():
-    for row in range(1,len(data)):
+    for row in range(1, len(data)):
         print(data[row][0])
         if data[row][0] in batchIDs:
             print("duplicate")
         else:
             batchIDs.append(data[row][0])
+
+
 ##            print(*batchIDs)
-    
+
 def checkValues():
     for row in data:
         for item in row:
-            if item == "" or item == " ":
+            if len(year) == 0 or year.isspace():
                 print("empty item")
             else:
                 if item.isdigit():
-                    bob=0
-##                    print("number")
+                    bob = 0
+                ##                    print("number")
                 elif item.isalnum():
-                    bob=0
-##                    print("string")
+                    bob = 0
+                ##                    print("string")
                 elif isFloat(item):
-                    bob=0
-##                    print("float")
+                    bob = 0
+                    ##                    print("float")
                     if float(item) > 9.9:
                         print("EXCEED")
                         print(item)
                         ##SCRAP FILE##
-                    
+
+def checkRounded3DP():
+    for row in data:
+        for item in row:
+            if len(year) == 0 or year.isspace():  # Check white space
+                print("Empty")
+            elif(format(item,".3f") == item):  #Check if 3dp
+                print("item")
+            else:
+                print("Scrapping File")
+                
